234ff7a06f3400aa8c46ed89f60f50d418d20b5a
EderPaulo <paulofiuza24@gmail.com>
2024-10-19 08:52:33 -0300

    sobrecarga de métodos

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace Principal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Adicao();
+
+            Console.WriteLine(calc.Somar(1, 2, 3));
+            Console.WriteLine(calc.Somar(1, 2));
+        }
+    }
+
+    public class Adicao
+    {
+        // Método para somar dois inteiros
+        public int Somar(int x, int y)
+        {
+            return x + y;
+        }
+
+        // Sobrecarga: método para somar três inteiros
+        public int Somar(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+    }
+}
